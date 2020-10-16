c22e0bb88884ce46179967ddf64c3a711e7ced73
Beatriz Lira <beatrizliragonzaga@gmail.com>
2020-10-16 16:05:56 -0300

    modificando rotas

--- src/server.js
+++ src/server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const path = require('path');
+const pages = require('./pages.js')
 const server = express();
 
 server
     .use(express.static('public'))
 
     // configurar template engine
-    .set('views', path.join(__dirname, "view"))
+    .set('views', path.join(__dirname, "views"))
     .set('view engine', 'hbs')
 
-// criar rota
-server.get('/', (request, response) => {
-    return response.render('index')
-})
+// rotas da aplicação
+server.get('/', pages.index)
+server.get('/orphanage', pages.orphanage)
+server.get('/orphanages', pages.orphanages)
+server.get('/create-orphanage', pages.createOrphanage)
 
 //  ligar servidor
 server.listen(5500)
