f7655522095562975cd838bc0d0531033d34b4a7
Beatriz Lira <beatrizliragonzaga@gmail.com>
2020-10-15 22:19:15 -0300

    Configurando container de imagens com js

---
+++ public/scripts/page-create-orphanage.js
@@ -0,0 +1,70 @@
+// create map
+const map = L.map('mapid').setView([-23.629721, -46.574714], 15);
+
+// create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+// create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+})
+
+let marker;
+
+// create and add marker 
+map.on('click', (event) => {
+    const lat = event.latlng.lat;
+    const lng = event.latlng.lng;
+
+    document.querySelector('[name=lat]').value = lat;
+    document.querySelector('[name=lng]').value = lng;
+
+    // remove icon
+    marker && map.removeLayer(marker)
+
+    // add icon layer
+    marker = L.marker([lat, lng], { icon })
+        .addTo(map)
+})
+
+// add photos 
+function addPhotoField() {
+    // pegar o container de fotos
+    const container = document.querySelector('#images')
+
+    // pegar o container para duplicar
+    const fieldsContainer = document.querySelectorAll('.new-upload')
+
+    // realizar o clone da última imagem adicionada
+    const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true)
+
+    // verificar se o campo está vazio, se sim, não adicionar ao container de imagens
+    const input = newFieldContainer.children[0]
+
+    if (input.value == '') {
+        return
+    }
+
+    // limpar o campo antes de adicionar a container de imagens
+    input.value = ''
+
+    // adicionar o clone ao container de #images
+    container.appendChild(newFieldContainer)
+}
+
+function deleteField(event) {
+    const span = event.currentTarget
+
+    const fieldsContainer = document.querySelectorAll('.new-upload')
+
+    if (fieldsContainer.length <= 1) {
+        // limpar o valor do campo
+        span.parentNode.children[0].value = ''
+        return
+    }
+
+    // deletar o campo
+    span.parentNode.remove();
+}
